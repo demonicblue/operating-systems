2d7101d4c3e3cf0b3e3747abcd4f04d28183b5a0
Frolle <frollix@razzan.net>
2013-11-26 18:28:42 +0100

    Implemented 2 locks.

---
+++ lab2/concurrent_queue_2locks.c
@@ -0,0 +1,44 @@
+#include "queue.h"
+
+void initialize_queue(void){
+	//Initialize dummy node
+	Node *dummy;
+	dummy = malloc(sizeof(struct node));
+	dummy->value=0;
+	dummy->next=NULL;
+	//Initialize queue
+	queue = malloc(sizeof(struct queue));
+	queue->head=dummy;
+	queue->tail=dummy;
+	//Initialize mutex variable
+	mutex1 = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+	mutex2 = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+}
+
+void enqueue(int val){
+	pthread_mutex_lock(&mutex1);
+	Node *new_tail;
+	new_tail = malloc(sizeof(struct node));
+	new_tail->value=val;
+	new_tail->next=NULL;
+
+	queue->tail->next = new_tail;
+	queue->tail = new_tail;
+	pthread_mutex_unlock(&mutex1);
+}
+
+int dequeue(int *extractedValue){
+	pthread_mutex_lock(&mutex2);
+	if(queue->head->next == NULL){
+		pthread_mutex_unlock(&mutex2);
+		return 0;
+	}
+	else{
+		*extractedValue = queue->head->next->value;
+		Node *temp = queue->head;
+		queue->head = queue->head->next;
+		free(temp);
+		pthread_mutex_unlock(&mutex2);
+		return 1;
+	}
+}
---
+++ lab2/concurrent_queue_2locks.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <pthread.h>
+typedef struct node{
+	int value;
+	struct node *next;
+} Node;
+
+typedef struct queue{
+	Node *head;
+	Node *tail;
+} Queue;
+
+Queue *queue;
+pthread_mutex_t mutex1;
+pthread_mutex_t mutex2;
+
+void initialize_queue(void);
+void enqueue(int val);
+int dequeue(int *extractedValue);
